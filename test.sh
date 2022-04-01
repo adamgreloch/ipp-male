b7437eb9dc2844b5e22829073f4f72c008795bf1
Adam Greloch <zplhatesbananas@gmail.com>
2022-04-01 17:55:00 +0200

    Improve test script

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+ans_passed=0
+ans_failed=0
+err_passed=0
+err_failed=0
+mem_errors=0
+
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 RESET='\033[0m'
@@ -12,47 +18,58 @@ do
     rm -f "${f%in}memerr" > /dev/null
 done
 
-ans_passed=0
-ans_failed=0
-err_passed=0
-err_failed=0
-mem_errors=0
+echo -e "\n### Program correctness test ###\n"
 
 time for f in "$2"/*.in
+do
+    ./"$1" <"$f" 1>"${f%in}outp" 2>"${f%in}errp"
+    echo "--- ${f#*/}"
+    if diff "${f%in}outp" "${f%in}out" > /dev/null 1>&1
+    then
+        echo -e "Answer ${GREEN}OK${RESET}"
+        rm -f "${f%in}outp" > /dev/null
+        ((ans_passed++))
+    else
+        echo -e "Answer ${RED}failed${RESET}!"
+        echo "Expected:"
+        comm -13 "${f%in}outp" "${f%in}out"
+        echo "Got:"
+        comm -23 "${f%in}outp" "${f%in}out"
+        ((ans_failed++))
+    fi
+
+    if diff "${f%in}errp" "${f%in}err" > /dev/null 1>&1
+    then
+        echo -e "Error ${GREEN}OK${RESET}"
+        rm -f "${f%in}errp" > /dev/null
+        ((err_passed++))
+    else
+        echo -e "Error ${RED}failed${RESET}!"
+        echo "Expected:"
+        comm -13 "${f%in}errp" "${f%in}err"
+        echo "Got:"
+        comm -23 "${f%in}errp" "${f%in}err"
+        ((err_failed++))
+    fi
+done
+
+echo -e "\n### Memory integrity test ###\n"
+
+for f in "$2"/*.in
 do
     valgrind --leak-check=full \
              --show-leak-kinds=all \
              --errors-for-leak-kinds=all \
              --quiet \
              --log-file="${f%in}memerr" \
-    ./"$1" <"$f" 1>"${f%in}outp" 2>"${f%in}errp"
+    ./"$1" <"$f" 1>/dev/null 2>/dev/null
 
-    echo -en "--- ${f#*/}\n";
-    if diff "${f%in}outp" "${f%in}out" > /dev/null 1>&1
-    then
-        echo -e "answer ${GREEN}OK${RESET}"
-        ((ans_passed++))
-    else
-        echo -e "answer ${RED}failed${RESET}:"
-        diff -y -W 50 "${f%in}outp" "${f%in}out"
-        ((ans_failed++))
-    fi
-
-    if diff "${f%in}errp" "${f%in}err" > /dev/null 1>&1
-    then
-        echo -e "error ${GREEN}OK${RESET}"
-        ((err_passed++))
-    else
-        echo -e "error ${RED}failed${RESET}:"
-        diff -y -W 50 "${f%in}errp" "${f%in}err"
-        ((err_failed++))
-    fi
-
+    echo "--- ${f#*/}";
     if [ ! -s "${f%in}memerr" ]
     then
-        echo -e "memory ${GREEN}OK${RESET}"
+        echo -e "Memory ${GREEN}OK${RESET}"
     else
-        echo -e "${RED}memory problems!${RESET}"
+        echo -e "${RED}Memory problems!${RESET}"
         cat "${f%in}memerr"
         ((mem_errors++))
     fi
@@ -61,8 +78,6 @@ done
 total=$((ans_passed+ans_failed))
 
 echo -e "\nIn total:"
-echo "Number of tests: $total"
-echo -e "Correct answers: $ans_passed"
-echo "Errors handled correctly: $err_passed"
+echo "Correct answers: $ans_passed/$total"
+echo "Errors handled correctly: $err_passed/$total"
 echo "Tests where memory issues occured: $mem_errors"
-
